5dc3826b7337939b73cb237a4d8e1762df322204
Kate Ly <Kate11168@Kates-MacBook-Air.local>
2016-01-04 22:13:04 -0500

    added student

---
+++ Student.java
@@ -0,0 +1,20 @@
+/*=============================================
+  class Student - subclass of Character
+  =============================================*/
+
+public class Student extends Character {
+
+    // ~~~~~~~~~~~ INSTANCE VARIABLES ~~~~~~~~~~~
+
+
+    // ~~~~~~~~~~~ CONSTRUCTORS ~~~~~~~~~~~
+
+    public Student(String n, String z) {
+	super(n,z);
+    }
+
+
+    // ~~~~~~~~~~~~~~ ACCESSORS ~~~~~~~~~~~~~~~~~
+
+
+} // end class Student
